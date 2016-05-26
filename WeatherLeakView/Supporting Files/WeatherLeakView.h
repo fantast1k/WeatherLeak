0de018a0d626c84598da18a4d2ae54905a7c5ee2
Dmitry Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-05-26 23:23:57 +0300

    Change basic structure for the project

---
+++ WeatherLeakView/Supporting Files/WeatherLeakView.h
@@ -0,0 +1,19 @@
+//
+//  WeatherLeakView.h
+//  WeatherLeakView
+//
+//  Created by Dmitry Fa[n]tastik on 26/05/2016.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WeatherLeakView.
+FOUNDATION_EXPORT double WeatherLeakViewVersionNumber;
+
+//! Project version string for WeatherLeakView.
+FOUNDATION_EXPORT const unsigned char WeatherLeakViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherLeakView/PublicHeader.h>
+
+
