3e0395b79caf23c08bd801131ada2bdfc41b636c
Dmitry Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-05-27 02:15:20 +0300

    Add WeatherLeakCore target

---
+++ WeatherLeakCore/Supporting Files/WeatherLeakCore.h
@@ -0,0 +1,19 @@
+//
+//  WeatherLeakCore.h
+//  WeatherLeakCore
+//
+//  Created by Dmitry Fa[n]tastik on 27/05/2016.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WeatherLeakCore.
+FOUNDATION_EXPORT double WeatherLeakCoreVersionNumber;
+
+//! Project version string for WeatherLeakCore.
+FOUNDATION_EXPORT const unsigned char WeatherLeakCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherLeakCore/PublicHeader.h>
+
+
